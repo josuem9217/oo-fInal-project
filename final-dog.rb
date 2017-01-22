69d3e3fd49a02c0d3a0f663da633802495e84b65
Josue <josuem9217@hstat.org>
2017-01-22 21:17:25 +0000

    one file

--- final-dog.rb
+++ final-dog.rb
@@ -1,8 +1,24 @@
+class Owner
+    attr_accessor :nameO, :dogs 
+    def initialize(nameO)
+        @nameO = nameO
+        @dogs = []
+        
+    end
+    
+    # def adoptAdog(doggo)
+    #     @doggo = doggo
+    #     @dogs << @dog
+    #     doggo.ownerAdopt << self
+    # end
+    
+end
+
 class Dog
-    attr_accessor :name, :owners, :size, :weight
+    attr_accessor :nameD, :owners, :size, :weight
     attr_reader :specie
-    def initialize(name, specie, size, weight)
-        @name = name
+    def initialize(nameD, specie, size, weight)
+        @nameD = nameD
         @owners = []
         @specie = specie
         @size = size
@@ -11,8 +27,17 @@ class Dog
         
     end
     
+    def kidFriendly
+      if @kidFriendly == 1 
+        @kidFriendly = "KID FRIENDLY!"
+      else
+        @kidFriendly = "NOT KID FRIENDLY!!!"
+      end
+      return @kidFriendly
+    end
+    
     def dogQualities
-       puts "#{@name}, #{@specie}, #{@size}, #{@weight}, #{@kidFriendly}"
+        "#{@nameD} is a  #{@specie}, who is quite #{@size}, and weighs #{@weight}!"
     end
         
         
@@ -24,6 +49,68 @@ class Dog
     
 end
 
-roof = Dog.new("roof")
-puts "What is your doggo's name?"
-name = gets.chomp
+# roof = Dog.new("roof")
+# puts "What is your doggo's name?"
+# name, dog = gets.chomp
+# puts "What is your doggo's specie?"
+# specie = gets.chomp
+# puts "What is your doggo's size?"
+# size = gets.chomp
+# puts "What is your doggo's weight?"
+# weight = gets.chomp
+# dog = Dog.new("#{name}", "#{specie}", "#{size}", "#{weight}")
+
+
+puts "What is your name?"
+nameO, owner = gets.chomp
+owner = Owner.new("#{nameO}")
+
+puts "What kind of doggo are you looking for?"
+puts  "What is your ideal specie?"
+specie = gets.chomp
+puts "What is your ideal size?"
+puts "Small? Big? Large?"
+size = gets.chomp
+
+weight = "5kg"
+
+puts "hmmmmm"
+puts "looks like we might have a doggo that fits that description"
+puts "Do you have a name in mind while we fetch the #{size} doggo?"
+nameD, dog = gets.chomp
+dog = Dog.new("#{nameD}", "#{specie}", "#{size}", "#{weight}")
+
+puts "Well allow me #{nameO}, to introduce you to #{nameD}!!!"
+puts dog.dogQualities
+
+
+puts "Do you want to know if your doggo is kid friendly?"
+answ1 = gets.chomp.downcase
+
+if answ1 == "yes"
+  dog.kidFriendly
+else 
+  puts "Nevermind then"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- final-owner.rb
+++ final-owner.rb
@@ -1,54 +1,15 @@
-class Dog
-    attr_accessor :name, :owners, :size, :weight
-    attr_reader :specie
-    def initialize(name, specie, size, weight)
+class Owner
+    attr_accessor :name, :dogs 
+    def initialize(name)
         @name = name
-        @owners = []
-        @specie = specie
-        @size = size
-        @weight = weight
-        @kidFriendly = srand[2]
+        @dogs = []
         
     end
     
-    def kidFriendly
-      if @kidFriendly == 1 
-        @kidFriendly = "KID FRIENDLY!"
-      else
-        @kidFriendly = "NOT KID FRIENDLY!!!"
-      end
-      return @kidFriendly
+    def adoptAdog(doggo)
+        @doggo = doggo
+        @dogs << @dog
+        doggo.ownerAdopt << self
     end
     
-    def dogQualities
-       return "#{@name}, #{@specie}, #{@size}, #{@weight}, #{@kidFriendly}"
-    end
-        
-        
-    # def ownerAdopt(owner)
-    #     @owner = owner
-    #     @owners << @owner
-    #     owner.adoptAdog << self
-    # end
-    
-end
-
-# roof = Dog.new("roof")
-puts "What is your doggo's name?"
-name, dog = gets.chomp
-puts "What is your doggo's specie?"
-specie = gets.chomp
-puts "What is your doggo's size?"
-size = gets.chomp
-puts "What is your doggo's weight?"
-weight = gets.chomp
-dog = Dog.new("#{name}", "#{specie}", "#{size}", "#{weight}")
-
-puts "Do you want to know if your doggo is kid friendly?"
-answ1 = gets.chomp.downcase
-
-if answ1 == "yes"
-  dog.kidFriendly
-else 
-  puts "Nevermind then"
 end
